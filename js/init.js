82ac9ee7d57c867558ab0c63825d5452417a5cab
Hilary Oba <israel.hilary7@yahoo.com>
2018-10-02 17:14:17 -0700

    First commit

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+        $(document).ready(function() {
+            $('.slider').slider({
+                full_width: true
+            });
+        });
---
+++ text.py
@@ -0,0 +1,8 @@
+ x = 2
+    y = 4
+    z = x + y 
+    print z
+    
+tu = (23, ‘abc’, 4.56, (2,3), ‘def’)
+fred = ['rice','beans','chicken',33,'lagos',['father':30,'mother':25]]
+print (fred[1])
